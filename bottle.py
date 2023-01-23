41184a9858d36a71fcc4f7927a0b0460df77be7a
Aimé Bea <72194058+davik13@users.noreply.github.com>
2023-01-23 12:35:52 +0100

    add all

--- bottle.py
+++ bottle.py
@@ -1,6 +1,15 @@
-from bottle import  run
+from bottle import route, run
+from calcul import calcul
 
+
+@route('/hello')
 def hello():
-    return "Hello"
+    return "Hello World! bien tu es sur la page hello"
+
+
+@route('/add/<a>/<b>')
+def add(a, b):
+    return {'result': calcul(a, b)}
+
 
 run(host='localhost', port=8080, debug=True, reloader=True)
---
+++ calcul.py
@@ -0,0 +1,8 @@
+def calcul(a, b):
+    """
+    >>> calcul(1, 2)
+    '3'
+    >>> calcul(2, 3)
+    '5'
+    """
+    return str(a + b)
