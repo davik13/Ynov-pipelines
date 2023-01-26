4f16ced398e66660701be6804c30068f2fc18b1a
Aimé Bea <72194058+davik13@users.noreply.github.com>
2023-01-26 11:38:17 +0100

    check

--- bottle.py
+++ bottle.py
@@ -6,12 +6,15 @@ from calcul import calcul
 def homepage():
     return 'Hello you !'
 
+
 @route('/hello/<name>')
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
+
 @route('/add/<a>/<b>')
 def add(a, b):
     return {'result': calcul(a, b)}
+
 
 run(host='localhost', port=8080, debug=True, reloader=True)
